b8a0792865662af8d29d95de4bc2e155e92a3b80
Arter Tendean <arter.tendean.07@gmail.com>
2020-09-15 09:17:17 +0800

    Belajar JavaScript

---
+++ Selasa, 15 September 2020/script.js
@@ -0,0 +1,19 @@
+console.log('Halo Dunia!');
+
+const h1 = document.querySelector("h1");
+h1.innerText = "Hello World!";
+
+// console.error("Ini adalah pesan error");
+// console.warn("Ini adalah peringatan");
+
+const name = 'Arter Tendean';
+const age = 100;
+const height = 50;
+const is_dead = false;
+const x = 1012;
+const y = 845;
+
+// ES6 tempalte literals
+console.log(`Nama saya ${name} dan saya berusia ${age} tahun`);
+
+
